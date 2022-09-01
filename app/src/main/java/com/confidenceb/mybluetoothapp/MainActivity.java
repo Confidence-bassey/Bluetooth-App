a7220c3d9cf437a34845d5be533ea34e1f612f4f
Confidence-bassey <confidencebass2@gmail.com>
2022-09-01 23:48:46 +0100

    Added a broadcast receiver, an intent filter and the actions needed

--- app/src/main/java/com/confidenceb/mybluetoothapp/MainActivity.java
+++ app/src/main/java/com/confidenceb/mybluetoothapp/MainActivity.java
@@ -20,11 +20,20 @@ public class MainActivity extends AppCompatActivity {
     ListView listView;
     TextView tview;
     Button searchBtn;
+    BluetoothAdapter bluetoothAdapter;
 
     public void searchbtnClicked(View view){
         tview.setText("Searching available devices...");
         searchBtn.setEnabled(false);
     }
+
+    private  final BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            Log.i("Action performed is: ", action);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +43,17 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.listview);
         tview = findViewById(R.id.deviceStatus);
         searchBtn = findViewById(R.id.searchbtn);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
+        intentFilter.addAction(BluetoothDevice.ACTION_FOUND);
+        intentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        intentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(receiver,intentFilter);
+
+        bluetoothAdapter.startDiscovery();
 
     }
 }
