71613a30a7e6b0b8426e99325abf88a2703dc00a
Confidence-bassey <confidencebass2@gmail.com>
2022-09-01 23:36:45 +0100

    Bluetooth app first commit

---
+++ app/src/main/java/com/confidenceb/mybluetoothapp/MainActivity.java
@@ -0,0 +1,39 @@
+package com.confidenceb.mybluetoothapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    TextView tview;
+    Button searchBtn;
+
+    public void searchbtnClicked(View view){
+        tview.setText("Searching available devices...");
+        searchBtn.setEnabled(false);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listview);
+        tview = findViewById(R.id.deviceStatus);
+        searchBtn = findViewById(R.id.searchbtn);
+
+    }
+}
